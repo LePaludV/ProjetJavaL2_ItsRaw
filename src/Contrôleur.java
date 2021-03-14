7e448433938aa970051637a6e472cf81b1f4c16d
GabrielCadilhac <59889782+GabrielCadilhac@users.noreply.github.com>
2021-03-14 16:17:04 +0100

    Gabriel
    Ajout du Modèle, du Contrôleur et des affichages de listes pour la Vue

---
+++ src/Contrôleur.java
@@ -0,0 +1,11 @@
+
+public class Contrôleur {
+	
+	Modèle mdl;
+	public Contrôleur(Modèle modèle) {
+		this.mdl = modèle;
+	}
+	
+	
+
+}
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import javafx.stage.Stage;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		Application.launch(Vue.class, args);
+		Application.launch(Vue.class);
 
 	}
 }
