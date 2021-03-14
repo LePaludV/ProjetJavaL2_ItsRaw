a39a1afd084924681a5b78f236e996062a165ab6
LePaludV <59608384+LePaludV@users.noreply.github.com>
2021-03-14 13:06:40 +0100

    Début Vue
    Création de la fenêtre dans la vue qui est lancé par le main, la classe accueil doit être appelé par la vue et envoyer la scène de l'accueil

---
+++ src/Accueil.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class Accueil {
+	Label label;
+	public Accueil(){
+		
+	}
+
+
+	
+		
+	
+		// TODO Auto-generated constructor stub
+	}
+			
+	
+		
+		
+	
+
+	
+
+	
+	
+
+	
+
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+<<<<<<< Updated upstream
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+=======
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Application.launch(Vue.class, args);
+
+>>>>>>> Stashed changes
 	}
 
 }
