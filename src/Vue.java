d2b9dc4efb9d913e586b069ffd67a27ffc9c9e79
GabrielCadilhac <59889782+GabrielCadilhac@users.noreply.github.com>
2021-03-20 09:50:38 +0100

    Gabriel
    Changement pour la méthode ajoutPhoto qui reçoit maintenant en paramètre un File

--- src/Vue.java
+++ src/Vue.java
@@ -1,24 +1,16 @@
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Vue extends Application {
 	Accueil acc=new Accueil();
-<<<<<<< Updated upstream
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("It's Raw");
-		StackPane root= new StackPane();
-//		root.getChildren().add(acc);
-		Scene scene = new Scene(root, 800, 600);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		
-=======
 	GridPane rootLayout;
 	
 	@Override
@@ -45,7 +37,6 @@ public class Vue extends Application {
     	  catch(IOException e) {
     		  e.printStackTrace();
     	  }
->>>>>>> Stashed changes
 	}
 	
 }
