a71d7c6273f806cf842262647906fbb1e121d903
GabrielCadilhac <59889782+GabrielCadilhac@users.noreply.github.com>
2021-03-20 10:03:20 +0100

    Update Vue.java
    Ajout d'une énumération pour différencier les interfaces

--- src/Vue.java
+++ src/Vue.java
@@ -12,31 +12,44 @@ import javafx.stage.Stage;
 public class Vue extends Application {
 	Accueil acc=new Accueil();
 	GridPane rootLayout;
+	private Stage primaryStage;
+	
+	private enum InterfaceType {
+			AJOUT_RECETTE, ACCUEIL, PRESENTATION_RECETTE, ETAPE_RECETTE
+	};
+	
+	private InterfaceType currentInterface = InterfaceType.AJOUT_RECETTE;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("It's Raw");
-        this.primaryStage.setWidth(600);
-        this.primaryStage.setHeight(600);
-		
-        this.primaryStage.sizeToScene();
-        
-        // Show the scene containing the root layout.
-    
-    	  try {
-    		  FXMLLoader loader = new FXMLLoader();
-              loader.setLocation(Vue.class.getResource("ajoutRecette.fxml"));
-              rootLayout = (GridPane) loader.load();
-              
-              Scene scene=new Scene(rootLayout);
-              primaryStage.setScene(scene);
-              primaryStage.show();
-    	  }
-    	  catch(IOException e) {
-    		  e.printStackTrace();
-    	  }
+		if (currentInterface == InterfaceType.AJOUT_RECETTE) {
+			this.primaryStage = primaryStage;
+	        this.primaryStage.setTitle("It's Raw");
+	        this.primaryStage.setWidth(600);
+	        this.primaryStage.setHeight(600);
+			
+	        this.primaryStage.sizeToScene();
+	        
+	    	  try {
+	    		  FXMLLoader loader = new FXMLLoader();
+	              loader.setLocation(Vue.class.getResource("ajoutRecette.fxml"));
+	              rootLayout = (GridPane) loader.load();
+	              
+	              Scene scene=new Scene(rootLayout);
+	              primaryStage.setScene(scene);
+	              primaryStage.show();
+	    	  }
+	    	  catch(IOException e) {
+	    		  e.printStackTrace();
+	    	  }
+		} else if (currentInterface == InterfaceType.ACCUEIL) {
+			
+		} else if (currentInterface == InterfaceType.PRESENTATION_RECETTE) {
+			
+		} else if (currentInterface == InterfaceType.ETAPE_RECETTE) {
+			
+		}
 	}
 	
 }
