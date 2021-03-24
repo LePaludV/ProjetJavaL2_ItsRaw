df354d01d4679a2274e810342ce7ed4c5d625772
mel421 <melvin.bertrand@etud.univ-jfc.fr>
2021-03-24 11:57:41 +0100

    Boutons
    J'ai mis des boutons avec image à la place de simple images pour les recettes
    (peu fonctionnel)

--- src/InterfaceAccueil.java
+++ src/InterfaceAccueil.java
@@ -1,15 +1,25 @@
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -53,12 +63,21 @@ public class InterfaceAccueil implements Observer
 			{
 				if(i+j<lstRecettes.size())
 				{
-					ImageView imageCourante = new ImageView(lstRecettes.get(i+j).photo);
+					Button btn = new Button();
+					/*ImageView imageCourante = new ImageView(lstRecettes.get(i+j).photo);
 					imageCourante.setFitHeight(100);
 					imageCourante.setFitWidth(100);
 					System.out.println("rec" + recettes);
-					System.out.println(lstRecettes.get(i+j).photo);
-					hb.getChildren().add(imageCourante);
+					System.out.println(lstRecettes.get(i+j).photo);*/
+					try {
+						btn.backgroundProperty().setValue(new Background((new BackgroundImage(new Image(new FileInputStream("imgs/lasagnes.jpg")), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, null, null))));
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					//btn.setOnAction(ctrlAccueil.ajouterRecette(new ActionEvent());
+					hb.getChildren().add(btn);
+					//hb.getChildren().add(imageCourante);
 				}
 			}
 			this.ctrlAccueil.recettes.getChildren().add(hb);
