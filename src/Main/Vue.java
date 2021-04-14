79e9e52a00a773bf14693956d39657f05a35643d
mel421 <melvin.bertrand@etud.univ-jfc.fr>
2021-04-14 11:22:19 +0200

    Design

--- src/Main/Vue.java
+++ src/Main/Vue.java
@@ -107,7 +107,6 @@ public class Vue extends Application {
             
         } else if (this.currentInterface == typeInterface.ETAPE_RECETTE) {
         	Recette rct = new Recette();
-        	System.out.println("ok");
         	rct = this.mdlAccueilRecette.recette_courante;
         	this.mdlEtapes = new ModèleEtapes(this);
         	this.mdlEtapes.setRecette(rct);
